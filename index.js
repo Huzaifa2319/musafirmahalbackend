2b5d8ef4a68d3142c7188314f8e2a0240cf093ea
Huzaifa Anjum <i202319@nu.edu.pk>
2024-03-10 22:38:43 +0500

    cors

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const app = express();
 require("dotenv").config();
 
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 const MONGODB_URL = process.env.URL;
 const port = process.env.PORT;
